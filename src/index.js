3cbbb1db85d1f940be80e5536b34e24ec87a3e17
Paserno <paserno.x@gmail.com>
2021-11-21 00:17:22 -0300

    Promise.race(), devuelve el elemento mas rapido

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
+import { promesaLenta, promesaMedia, promesaRapida} from './js/promesas';
 
-import './styles.css';
+
+
+// promesaLenta.then(mensaje => console.log(mensaje));
+// promesaMedia.then(console.log);
+// promesaRapida.then(console.log);
+
+//? Promise.race(), devuelve el elemento mas rapido que se ejecute, ignorando los demas
+Promise.race([ promesaLenta, promesaMedia, promesaRapida])
+        .then(console.log)
+        .catch(console.warn);
+
--- src/js/promesas.js
+++ src/js/promesas.js
@@ -31,3 +31,20 @@ export const buscarHeroe = ( id )=> {
     });
 
 }
+
+
+const  promesaLenta = new Promise((resolve,reject)=>{
+    setTimeout(() => resolve('Promesa Lenta'), 2000);
+});
+const  promesaMedia = new Promise((resolve,reject)=>{
+    setTimeout(() => resolve('Promesa Media'), 1500);
+});
+const  promesaRapida = new Promise((resolve,reject)=>{
+    setTimeout(() => reject('Promesa Rapida'), 1000);
+});
+
+export {
+    promesaLenta,
+    promesaMedia,
+    promesaRapida
+}
