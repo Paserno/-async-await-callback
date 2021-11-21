c4faec7317c807d0de10a2f96205caad37c800f3
Paserno <paserno.x@gmail.com>
2021-11-20 22:09:04 -0300

    Update index.js
    Callback hell, es la anidacion de multiples callback, el cual provoca que sea dificil de leer, para esto en el ECMScrpt 6 nacio la Promesa...

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
+//? Callback hell, es la anidacion de muchos callback "dificil de leer"
 import { buscarHeroe } from './js/callbacks';
 import './styles.css';
 
-const heroeID = 'hulk';
+const heroeID1 = 'spider';
+const heroeID2 = 'iron';
+const heroeID3 = 'hulk';
 
 //! Funcion como argumento
-buscarHeroe( heroeID, (err, heroe) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.info(heroe);
-    }
-} );
+buscarHeroe(heroeID1, (err, heroe1) => {
+    if (err) { return console.error(err); }
+
+    buscarHeroe(heroeID2, (err, heroe2) => {
+
+        if (err) { return console.error(err); }
+        console.log(`Eviando a ${heroe1.nombre} y ${heroe2.nombre} a la mision`);
+    
+
+    });
+});
 
